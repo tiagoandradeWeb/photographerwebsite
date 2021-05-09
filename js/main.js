80d37c2119d3465db2bfa08ab8d88598657b553a
Tiago <andrade.tiago166@gmail.com>
2021-05-09 10:38:16 -0300

    Site Completo adicionado com o resposnivo apenas para telas menores que 550px

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + (index + 1) + "</span>";
+      },
+    },
+    breakpoints:{
+      1000:{
+        slidesPerView: 3,
+      }
+    }
+  });
